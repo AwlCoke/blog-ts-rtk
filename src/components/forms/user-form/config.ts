ca5985a5fc50bcc56c9b7a3bff8ca6992fe0ca9d
AwlCoke <virulentia@gmail.com>
2020-11-06 10:15:32 +0300

    regex

--- src/components/forms/user-form/config.ts
+++ src/components/forms/user-form/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import { nanoid } from 'nanoid';
 import { UserFormValidProps } from '../../../types/forms';
 
@@ -139,7 +140,16 @@ export const ProfileFormProps: UserFormValidProps[] = [
     responseError: null,
     rules: {
       required: true,
-      pattern: /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/,
+      validate: {
+        url: (data) =>
+          data.match(
+            /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/i
+          ),
+        dataUrl: (data) =>
+          data.match(
+            /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i
+          ),
+      },
     },
   },
 ];
